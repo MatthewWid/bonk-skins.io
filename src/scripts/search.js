2cc408684d25a98a4913f5171b37a72482b9c28a
Matthewmob <game1000@hotmail.co.uk>
2017-08-21 14:39:27 +1000

    Search highlighting
    - Added feature where the matched query in search results will be highlighted.

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -16,3 +16,13 @@ document.getElementById("search-button").addEventListener("click", sendSearch);
 searchEl.addEventListener("click", function() {
 	this.value = "";
 });
+
+var url_obj = new URL(location.href);
+var query = url_obj.searchParams.get("q").toLowerCase();
+var cardTextList = document.querySelectorAll(".card .under p");
+
+for (var i = 0; i < cardTextList.length; i++) {
+	// https://stackoverflow.com/a/280824/2954591
+	var reg = new RegExp("(" + query + ")", "gi");
+	cardTextList[i].innerHTML = cardTextList[i].innerHTML.replace(reg, "<span style=\"background-color: rgb(254, 237, 87);\">$1</span>");
+}
